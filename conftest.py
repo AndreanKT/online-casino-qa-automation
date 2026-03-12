11ef73f4bc5dfcaa146bbf67b80097fc3fd4096e
Andrean <andreantahchiev@gmail.comgit config --global user.name>
2026-03-13 01:24:42 +0200

    DEV-10 Fix headless mode for CI/CD

--- conftest.py
+++ conftest.py
@@ -24,7 +24,7 @@ def auth_token(api_client):
 @pytest.fixture(scope="session")
 def browser():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         yield browser
         browser.close()
 
